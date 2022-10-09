1303f79028b259f19ee754a8b7815e3586769c60
Gumanpratapsingh <gumanpratapsinghparmar@gmail.com>
2022-10-09 18:48:44 +0530

    updated with the class and push and pop method

--- Data-Structures/stack/stack.py
+++ Data-Structures/stack/stack.py
@@ -1,22 +1,31 @@
-stack = []
-
-# append() function to push
-# element in the stack
-stack.append('g')
-stack.append('f')
-stack.append('g')
-
-print('Initial stack')
-print(stack)
-
-# pop() function to pop
-# element from stack in
-# LIFO order
-print('\nElements popped from stack:')
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
-
-print('\nStack after elements are popped:')
-print(stack)
-
+class Stack:
+    def __init__(self):
+        self.items = []
+ 
+    def is_empty(self):
+        return self.items == []
+ 
+    def push(self, data):
+        self.items.append(data)
+ 
+    def pop(self):
+        return self.items.pop()
+ 
+ 
+s = Stack()
+while True:
+    print('push <value>')
+    print('pop')
+    print('quit')
+    do = input('What would you like to do? ').split()
+ 
+    operation = do[0].strip().lower()
+    if operation == 'push':
+        s.push(int(do[1]))
+    elif operation == 'pop':
+        if s.is_empty():
+            print('Stack is empty.')
+        else:
+            print('Popped value: ', s.pop())
+    elif operation == 'quit':
+        break
